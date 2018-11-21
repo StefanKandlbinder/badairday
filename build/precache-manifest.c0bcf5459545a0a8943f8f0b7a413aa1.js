2e81ca1c67486b3667ab6f3d38274b5ff5aa3840
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2018-11-21 20:10:55 +0100

    refactoring

---
+++ build/precache-manifest.c0bcf5459545a0a8943f8f0b7a413aa1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "46782a7e7de2267e5210f6e31cc0ba25",
+    "url": "/static/media/Tab 2.46782a7e.m4a"
+  },
+  {
+    "revision": "c217bc9a3088ea2c81515bbe46dcefa8",
+    "url": "/static/media/Tab 1.c217bc9a.m4a"
+  },
+  {
+    "revision": "03b1300fa37b529f50a51d73da1213c8",
+    "url": "/static/media/Button 7.03b1300f.m4a"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4aa8e83f1641b297926b",
+    "url": "/static/js/main.4aa8e83f.chunk.js"
+  },
+  {
+    "revision": "621f0d750bce50712695",
+    "url": "/static/js/1.621f0d75.chunk.js"
+  },
+  {
+    "revision": "4aa8e83f1641b297926b",
+    "url": "/static/css/main.58ad5586.chunk.css"
+  },
+  {
+    "revision": "878e33b8e105249dd1921b35fc5f054c",
+    "url": "/index.html"
+  }
+];
