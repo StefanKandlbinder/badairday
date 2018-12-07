5755cb656094dd5850b0c6859891f4706a451dc7
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2018-12-07 22:35:53 +0100

    refactoring

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -74,7 +74,7 @@ class App extends Component {
     clearState();
 
     this.timeoutID = window.setTimeout(
-      () => { 
+      () => {
         this.props.history.push("/");
         window.location.reload(true);
       }, 2000);
@@ -108,8 +108,7 @@ class App extends Component {
   }
 
   render() {
-    let stations = null;
-    let station = null;
+    let map = null;
     let sidebar = null;
     let optionsSheet = null;
     let dashboard = null;
@@ -117,14 +116,6 @@ class App extends Component {
     let updateBar = null;
     let background = null;
     let app = null;
-
-    stations = <Stations />
-
-    station = <Route
-      path="/station/:provider/:id"
-      render={() => 
-        <Station />
-      } />
 
     sidebar = <Sidebar>
       <Spacer />
@@ -149,14 +140,14 @@ class App extends Component {
       <Flex className="air__flex air__flex--align-items-flex-end air__padding-left--3 air__padding-right--3 air__padding-top--4">
         <svg className="air__margin-right" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
           <linearGradient id="c" gradientUnits="userSpaceOnUse" x1="12" y1="3.0012" x2="12" y2="20.9988">
-            <stop offset="0" stopColor="#dd1800"/>
-            <stop offset=".5143" stopColor="#eeb600"/>
-            <stop offset="1" stopColor="#00796b"/>
+            <stop offset="0" stopColor="#dd1800" />
+            <stop offset=".5143" stopColor="#eeb600" />
+            <stop offset="1" stopColor="#00796b" />
           </linearGradient>
-          <path d="M12 4.2c.2 0 .3.1.4.3l7.3 14.6c.2.3-.1.7-.4.7-.1 0-.1 0-.2-.1l-6.9-3.4h-.4l-6.9 3.4c-.1 0-.1.1-.2.1-.3 0-.6-.3-.4-.7l7.3-14.6c.1-.2.2-.3.4-.3M7 17.4c.1 0 .1 0 .2-.1l4.5-2.2c.2-.1.3-.2.3-.4V7.9c0-.3-.2-.5-.5-.5-.2 0-.3.1-.4.3l-4.5 9c-.1.3.1.7.4.7M12 3c-.6 0-1.2.4-1.5.9L3.2 18.5c-.3.5-.2 1.1.1 1.6.3.6.8.9 1.4.9.3 0 .5-.1.8-.2l6.5-3.3 6.5 3.3c.2.1.5.2.8.2.6 0 1.1-.3 1.4-.8.3-.5.3-1.1.1-1.6L13.5 3.9c-.3-.5-.9-.9-1.5-.9zM8.8 15.2l2-4v3l-2 1z" fill="url(#c)"/>
+          <path d="M12 4.2c.2 0 .3.1.4.3l7.3 14.6c.2.3-.1.7-.4.7-.1 0-.1 0-.2-.1l-6.9-3.4h-.4l-6.9 3.4c-.1 0-.1.1-.2.1-.3 0-.6-.3-.4-.7l7.3-14.6c.1-.2.2-.3.4-.3M7 17.4c.1 0 .1 0 .2-.1l4.5-2.2c.2-.1.3-.2.3-.4V7.9c0-.3-.2-.5-.5-.5-.2 0-.3.1-.4.3l-4.5 9c-.1.3.1.7.4.7M12 3c-.6 0-1.2.4-1.5.9L3.2 18.5c-.3.5-.2 1.1.1 1.6.3.6.8.9 1.4.9.3 0 .5-.1.8-.2l6.5-3.3 6.5 3.3c.2.1.5.2.8.2.6 0 1.1-.3 1.4-.8.3-.5.3-1.1.1-1.6L13.5 3.9c-.3-.5-.9-.9-1.5-.9zM8.8 15.2l2-4v3l-2 1z" fill="url(#c)" />
         </svg>
         <h3 className="air__letter-spacing air__color-primary air__margin-bottom--0 air__margin-top--0">BadAirDay</h3>
-        </Flex>
+      </Flex>
       <ListHeader className="air__list-header air__list-header--sticky air__color-primary--active">Einstellungen</ListHeader>
       <List className="air__list">
         <ListItem className="air__list-item air__flex--justify-content-space-between">
@@ -180,7 +171,7 @@ class App extends Component {
           </Toggle>
         </ListItem>
         <ListItem className="air__list-item air__flex--justify-content-space-between">
-          <Toggle className={`air__toggle ${this.props.options.autoupdating ? "air__toggle--active" : "air__toggle--inactive"}`} 
+          <Toggle className={`air__toggle ${this.props.options.autoupdating ? "air__toggle--active" : "air__toggle--inactive"}`}
             clicked={() => this.props.onSetOptionAutoupdater({ state: this.props.options.autoupdating ? false : true, feature: STATIONS })}>
             Autoupdater
           </Toggle>
@@ -218,14 +209,14 @@ class App extends Component {
         className={`air__tabbar-link air__button air__button--naked ${!getFavorizedStations(this.props.stations).length ? "air__button--inactive" : ""}`}
         activeClassName="air__button--active"
         to={"/dashboard"}>
-        <svg className ="air__button-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 8.4h14.3v2.4H3V8.4zm0-4.7h14.3v2.4H3V3.7zm0 9.5h9.5v2.4H3v-2.4zM17.4 12.1l1.1 2.1.1.1 2.4.3c.1 0 .1.1.1.1v.1l-1.7 1.7v.1l.4 2.3c0 .1 0 .1-.1.2h-.1L17.3 18h-.1l-2.1 1.1c-.1 0-.1 0-.2-.1v-.1l.4-2.3v-.1l-1.7-1.7c-.1-.1-.1-.1 0-.2h.1l2.4-.3s.1 0 .1-.1l1.1-2.1c-.1-.1 0-.1.1 0-.1-.1 0-.1 0 0zM3 17.9h10.9v2.4H3z"/></svg>
+        <svg className="air__button-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none" /><path d="M3 8.4h14.3v2.4H3V8.4zm0-4.7h14.3v2.4H3V3.7zm0 9.5h9.5v2.4H3v-2.4zM17.4 12.1l1.1 2.1.1.1 2.4.3c.1 0 .1.1.1.1v.1l-1.7 1.7v.1l.4 2.3c0 .1 0 .1-.1.2h-.1L17.3 18h-.1l-2.1 1.1c-.1 0-.1 0-.2-.1v-.1l.4-2.3v-.1l-1.7-1.7c-.1-.1-.1-.1 0-.2h.1l2.4-.3s.1 0 .1-.1l1.1-2.1c-.1-.1 0-.1.1 0-.1-.1 0-.1 0 0zM3 17.9h10.9v2.4H3z" /></svg>
       </NavLink>
       <Spacer className="air__bg-color-text" />
       <NavLink
         className="air__tabbar-link air__button air__button--naked"
         activeClassName="air__button--active"
         to={this.props.history.location.pathname === "/bottomsheet" ? "/" : "/bottomsheet"}>
-        <svg className ="air__button-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" /></svg>
+        <svg className="air__button-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" /></svg>
       </NavLink>
     </Tabbar>
 
@@ -242,110 +233,119 @@ class App extends Component {
         this.props.location.lng + ",50";
     }
 
-    if (this.props.stations) {
-      stations = <Stations stations={this.props.stations} options={this.props.options} />;
-    }
-
     background = <div className="air__background">
-      <svg className="air__background-svg" xmlns="http://www.w3.org/2000/svg" version="1.1" id="Layer_1" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 600 600">
-        <path d="M289.3 103.1L98.7 484.2c-5.2 10.3 5.8 21.3 16.1 16.1l179.8-89.9c3.4-1.7 7.4-1.7 10.7 0l179.8 89.9c10.3 5.2 21.3-5.8 16.1-16.1L310.7 103.1c-4.4-8.9-17-8.9-21.4 0zm4.1 276.4l-117.2 58.6c-10.3 5.2-21.3-5.8-16.1-16.1l117.2-234.3c5.7-11.3 22.7-7.3 22.7 5.4v175.8c0 4.4-2.6 8.6-6.6 10.6z" fill="#fff" /></svg>
+      <svg className="air__background-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <linearGradient id="c" gradientUnits="userSpaceOnUse" x1="12" y1="3.0012" x2="12" y2="20.9988">
+          <stop offset="0" stopColor="#dd1800" />
+          <stop offset=".5143" stopColor="#eeb600" />
+          <stop offset="1" stopColor="#00796b" />
+        </linearGradient>
+        <path d="M12 4.2c.2 0 .3.1.4.3l7.3 14.6c.2.3-.1.7-.4.7-.1 0-.1 0-.2-.1l-6.9-3.4h-.4l-6.9 3.4c-.1 0-.1.1-.2.1-.3 0-.6-.3-.4-.7l7.3-14.6c.1-.2.2-.3.4-.3M7 17.4c.1 0 .1 0 .2-.1l4.5-2.2c.2-.1.3-.2.3-.4V7.9c0-.3-.2-.5-.5-.5-.2 0-.3.1-.4.3l-4.5 9c-.1.3.1.7.4.7M12 3c-.6 0-1.2.4-1.5.9L3.2 18.5c-.3.5-.2 1.1.1 1.6.3.6.8.9 1.4.9.3 0 .5-.1.8-.2l6.5-3.3 6.5 3.3c.2.1.5.2.8.2.6 0 1.1-.3 1.4-.8.3-.5.3-1.1.1-1.6L13.5 3.9c-.3-.5-.9-.9-1.5-.9zM8.8 15.2l2-4v3l-2 1z" fill="url(#c)" />
+      </svg>
     </div>;
 
-    background = null;
+    if (this.props.stations) {
+      map = <React.Fragment>
+        <Route
+        path="/station/:provider/:id"
+        render={() =>
+          <Station />
+        } />
+        <Stations stations={this.props.stations} options={this.props.options} />;
+      </React.Fragment>
 
-    app = <React.Fragment>
-      {station}
-      {stations}
-    </React.Fragment>
+      app = <React.Fragment>
+        <CSSTransition
+          in={this.state.hasStations}
+          timeout={300}
+          classNames="air__animation-fade"
+          mountOnEnter
+          unmountOnExit>
+          {map}
+        </CSSTransition>
+
+        <CSSTransition
+          in={this.props.history.location.pathname === "/dashboard" ? true : false}
+          classNames="air__animation-site-transition"
+          timeout={300}
+          mountOnEnter
+          unmountOnExit>
+          {dashboard}
+        </CSSTransition>
+
+        <Button
+          className="air__button air__button--naked air__button--fab air__button-location"
+          clicked={() => this.handleLocation()}>
+          <svg className="air__color-primary" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V2c0-.55-.45-1-1-1s-1 .45-1 1v1.06C6.83 3.52 3.52 6.83 3.06 11H2c-.55 0-1 .45-1 1s.45 1 1 1h1.06c.46 4.17 3.77 7.48 7.94 7.94V22c0 .55.45 1 1 1s1-.45 1-1v-1.06c4.17-.46 7.48-3.77 7.94-7.94H22c.55 0 1-.45 1-1s-.45-1-1-1h-1.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z" /></svg>
+        </Button>
+
+        <Legend />
+
+        {tabbar}
+
+        <CSSTransition
+          in={this.props.history.location.pathname === "/sidebar" ? true : false}
+          classNames="air__animation-sidebar"
+          timeout={300}
+          mountOnEnter
+          unmountOnExit>
+          {sidebar}
+        </CSSTransition>
+
+        <CSSTransition
+          in={this.props.history.location.pathname === "/bottomsheet" ? true : false}
+          classNames="air__animation-bottom-sheet"
+          timeout={300}
+          mountOnEnter
+          unmountOnExit>
+          {optionsSheet}
+        </CSSTransition>
+
+        {updateBar}
+
+        <CSSTransition
+          in={this.props.updating}
+          classNames="air__animation-fade"
+          timeout={300}
+          mountOnEnter
+          unmountOnExit>
+          <Spinner />
+        </CSSTransition>
+
+        <CSSTransition
+          in={this.props.loading}
+          classNames="air__animation-fade-crunchy"
+          timeout={300}
+          mountOnEnter
+          unmountOnExit>
+          <Spinner />
+        </CSSTransition>
+
+        <CSSTransition
+          in={this.props.geolocation}
+          classNames="air__animation-fade-crunchy"
+          timeout={150}
+          mountOnEnter
+          unmountOnExit>
+          <Loading>Geolocation</Loading>
+        </CSSTransition>
+
+        <CSSTransition
+          in={this.props.notifications.length > 0}
+          classNames="air__animation-notification"
+          timeout={300}
+          mountOnEnter
+          unmountOnExit>
+          <Notifications notifications={this.props.notifications} />
+        </CSSTransition>
+      </React.Fragment>
+    }
 
     return (
       <PageVisibility onChange={this.handleVisibilityChange}>
         <div className="air">
           {background}
-
-          <CSSTransition
-            in={this.state.hasStations}
-            timeout={300}
-            classNames="air__animation-fade"
-            mountOnEnter
-            unmountOnExit>
-            {app}
-          </CSSTransition>
-
-          <CSSTransition
-            in={this.props.history.location.pathname === "/dashboard" ? true : false}
-            classNames="air__animation-site-transition"
-            timeout={300}
-            mountOnEnter
-            unmountOnExit>
-            {dashboard}
-          </CSSTransition>
-
-          <Button
-            className="air__button air__button--naked air__button--fab air__button-location"
-            clicked={() => this.handleLocation()}>
-            <svg className="air__color-primary" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z" /><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V2c0-.55-.45-1-1-1s-1 .45-1 1v1.06C6.83 3.52 3.52 6.83 3.06 11H2c-.55 0-1 .45-1 1s.45 1 1 1h1.06c.46 4.17 3.77 7.48 7.94 7.94V22c0 .55.45 1 1 1s1-.45 1-1v-1.06c4.17-.46 7.48-3.77 7.94-7.94H22c.55 0 1-.45 1-1s-.45-1-1-1h-1.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z" /></svg>
-          </Button>
-
-          <Legend />
-
-          {tabbar}
-
-          <CSSTransition
-            in={this.props.history.location.pathname === "/sidebar" ? true : false}
-            classNames="air__animation-sidebar"
-            timeout={300}
-            mountOnEnter
-            unmountOnExit>
-            {sidebar}
-          </CSSTransition>
-
-          <CSSTransition
-            in={this.props.history.location.pathname === "/bottomsheet" ? true : false}
-            classNames="air__animation-bottom-sheet"
-            timeout={300}
-            mountOnEnter
-            unmountOnExit>
-            {optionsSheet}
-          </CSSTransition>
-
-          {updateBar}
-
-          <CSSTransition
-            in={this.props.updating}
-            classNames="air__animation-fade"
-            timeout={300}
-            mountOnEnter
-            unmountOnExit>
-            <Spinner />
-          </CSSTransition>
-
-          <CSSTransition
-            in={this.props.loading}
-            classNames="air__animation-fade-crunchy"
-            timeout={300}
-            mountOnEnter
-            unmountOnExit>
-            <Spinner />
-          </CSSTransition>
-
-          <CSSTransition
-            in={this.props.geolocation}
-            classNames="air__animation-fade-crunchy"
-            timeout={150}
-            mountOnEnter
-            unmountOnExit>
-            <Loading>Geolocation</Loading>
-          </CSSTransition>
-
-          <CSSTransition
-            in={this.props.notifications.length > 0}
-            classNames="air__animation-notification"
-            timeout={300}
-            mountOnEnter
-            unmountOnExit>
-            <Notifications notifications={this.props.notifications} />
-          </CSSTransition>
+          {app}
         </div>
       </PageVisibility>
     );
