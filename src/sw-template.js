01cbd3e5c5a54b4c89bb16a34a77f5c4bafb29af
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-07-07 15:13:07 +0200

    map control

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,66 +1,65 @@
 if ('function' === typeof importScripts) {
-    importScripts(
-      'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
-    );
-    /* global workbox */
-    if (workbox) {
-      console.log('Workbox is loaded');
-   
-      /* injection point for manifest files.  */
-      workbox.precaching.precacheAndRoute([]);
-   
-  /* custom cache rules*/
-  workbox.routing.registerNavigationRoute('/index.html', {
-        blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-      });
-   
-  workbox.routing.registerRoute(
-        /\.(?:png|gif|jpg|jpeg)$/,
-        workbox.strategies.cacheFirst({
-          cacheName: 'images',
-          plugins: [
-            new workbox.expiration.Plugin({
-              maxEntries: 60,
-              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-            }),
-          ],
-        })
-      );
-   
-  } else {
-      console.log('Workbox could not be loaded. No Offline support');
-    }
-  }
+  importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+  );
+  /* global workbox */
+  if (workbox) {
+    console.log('Workbox is loaded');
 
-  workbox.routing.registerRoute(
-    new RegExp('https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/.*', 'g'),
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'api-cache',
-      plugins: [
-        new workbox.cacheableResponse.Plugin({
-          statuses: [0, 200]
-        }),
-        new workbox.expiration.Plugin({
-          maxEntries: 1000,
-          maxAgeSeconds: 365 * 24 * 60 * 60
-        })
-      ]
-    })
-  );
-  
-  workbox.routing.registerRoute(
-    new RegExp('https://api.luftdaten.info/v1/sensor/.*', 'g'),
-    new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'api-cache',
-      plugins: [
-        new workbox.cacheableResponse.Plugin({
-          statuses: [0, 200]
-        }),
-        new workbox.expiration.Plugin({
-          maxEntries: 1000,
-          maxAgeSeconds: 3 * 60
-        })
-      ]
-    })
-  );
-  
+    /* injection point for manifest files.  */
+    workbox.precaching.precacheAndRoute([]);
+
+    /* custom cache rules*/
+    workbox.routing.registerNavigationRoute('/index.html', {
+      blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
+    });
+
+    workbox.routing.registerRoute(
+      /\.(?:png|gif|jpg|jpeg)$/,
+      workbox.strategies.cacheFirst({
+        cacheName: 'images',
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 60,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          }),
+        ],
+      })
+    );
+
+  } else {
+    console.log('Workbox could not be loaded. No Offline support');
+  }
+}
+
+workbox.routing.registerRoute(
+  new RegExp('https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/.*', 'g'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'api-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxEntries: 1000,
+        maxAgeSeconds: 365 * 24 * 60 * 60
+      })
+    ]
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://api.luftdaten.info/v1/sensor/.*', 'g'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'api-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxEntries: 1000,
+        maxAgeSeconds: 3 * 60
+      })
+    ]
+  })
+);
