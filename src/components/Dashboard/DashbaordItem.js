3d392d42584ec696490255c62447093d8e5ab651
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-07-08 21:24:16 +0200

    refactoring

--- src/components/Dashboard/DashbaordItem.js
+++ src/components/Dashboard/DashbaordItem.js
@@ -108,9 +108,9 @@ class DashboardItem extends Component {
                 </div>
             </div>
             <div className="air__dashboard-item-content air__dashboard-item-content--component">
-                {no2}
                 {pm10}
                 {pm25}
+                {no2}
             </div>
         </li>
     }
