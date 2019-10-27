2799fbdbfdf836bd623a06e5470f9ded6013a51d
Stefan Kandlbinder <stefan.kandlbinder@gmail.com>
2019-10-27 09:00:17 +0100

    Update getStringDateLuftdaten.js

--- src/utilities/getStringDateLuftdaten.js
+++ src/utilities/getStringDateLuftdaten.js
@@ -5,7 +5,7 @@ export default function getStringDateLuftdaten(date) {
     var newDate = getUnixDateFromLuftdaten(date);
     
     // add 1 hour because of timezone
-    newDate += 3600;
+    // newDate += 3600;
 
     return getStringDate(newDate * 1000);
 }
